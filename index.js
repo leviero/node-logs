402e9e3f46d314fbff933e124fe7333bf444f05f
leviero <leviero@lalamove.com>
2018-01-11 01:02:09 +0800

    UPT: move getLogger function inside logger function to make it private

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 'use strict'
-function getLogger(level) {
-    return function (message, fields, src_file, src_line, backtrace) {
-        let data = {
-            message,
-            level,
-            fields,
-            src_file,
-            src_line,
-            time: (new Date()).toISOString()
-        }
-
-        if (backtrace) data.backtrace = backtrace
-
-        console.log(JSON.stringify(data))
-        return data
-    }
-}
-
 function logger() {
     return {
         info: getLogger('info'),
@@ -24,6 +6,25 @@ function logger() {
         warning: getLogger('warning'),
         error: getLogger('error'),
         fatal: getLogger('fatal')
+    }
+
+    // private function
+    function getLogger(level) {
+        return function (message, fields, src_file, src_line, backtrace) {
+            let data = {
+                message,
+                level,
+                fields,
+                src_file,
+                src_line,
+                time: (new Date()).toISOString()
+            }
+
+            if (backtrace) data.backtrace = backtrace
+
+            console.log(JSON.stringify(data))
+            return data
+        }
     }
 }
 
