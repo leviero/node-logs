b7e1bc375f25baa14738a976e9393a114f434ff1
leviero <leviero@lalamove.com>
2018-01-15 11:18:57 +0800

    TST: use let instead of var

--- test.js
+++ test.js
@@ -1,21 +1,21 @@
 'use strict'
 const path = require('path')
 const tap = require('tap')
-const logger = require('./index.js')
+const log = require('./index.js')()
 
 if (typeof __stack === 'undefined') {
     Object.defineProperty(global, '__stack', {
         get: function() {
-            var orig
+            let orig
             try {
                 orig = Error.prepareStackTrace
                 Error.prepareStackTrace = function(_, stack) {
                     return stack
                 }
-                var err = new Error()
+                let err = new Error()
                 Error.captureStackTrace(err, arguments.callee)
             } catch (err) {
-                var stack = err.stack
+                let stack = err.stack
                 Error.prepareStackTrace = orig
                 return stack
             }
@@ -30,19 +30,19 @@ if (typeof __stack === 'undefined') {
 }
 
 tap.test('get logger', function(childTest) {
-    let log = logger()
     let keys = Object.keys(log)
+
     childTest.same(keys, ['info', 'debug', 'warning', 'error', 'fatal'])
     childTest.end()
 })
 
 tap.test('check log level', function(childTest) {
-    let log = logger()
     let infoLog = log.info('INFO', {}, path.relative(process.cwd(), __filename), __line)
     let debugLog = log.debug('DEBUG', {}, path.relative(process.cwd(), __filename), __line)
     let warningLog = log.warning('WARNING', {}, path.relative(process.cwd(), __filename), __line)
     let errorLog = log.error('ERROR', {}, path.relative(process.cwd(), __filename), __line)
     let fatalLog = log.fatal('FATAL', {}, path.relative(process.cwd(), __filename), __line)
+
     childTest.equal(infoLog.level, 'info')
     childTest.equal(debugLog.level, 'debug')
     childTest.equal(warningLog.level, 'warning')
@@ -52,8 +52,6 @@ tap.test('check log level', function(childTest) {
 })
 
 tap.test('check log format', function(childTest) {
-    let log = logger()
-
     const message = 'ERROR'
     const fields = {}
     const src_file = path.relative(process.cwd(), __filename)
@@ -61,11 +59,11 @@ tap.test('check log format', function(childTest) {
     const backtrace = {}
 
     let errorLog = log.info(message, fields, src_file, src_line, backtrace)
+
     childTest.equal(errorLog.message, message)
     childTest.equal(errorLog.fields, fields)
     childTest.equal(errorLog.src_file, src_file)
     childTest.equal(errorLog.src_line, src_line)
     childTest.equal(errorLog.backtrace, backtrace)
-
     childTest.end()
 })
