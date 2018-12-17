eb6c50894ff98f259a7f6b8b43e1dd9d08738c1b
Edbert <edbert.puspito@lalamove.com>
2018-12-17 15:02:59 +0800

    FIX:comments

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logger() {
             let callerInfo = getCallerFileAndLine()
 
             // normally, request and response in the context object can be a huge object.
-            // when the logs are processed by kubernetes, it result to thousands of fields depending on your application
-            // Thus results in performance penalty from the indexs. thus on e solution is to espace it and let k8s treat it as string
+            // when the logs are processed by Elasticsearch, it result in thousands of fields depending on your application
+            // Thus results in performance penalty from the indexes. thus the solution is to escape it and let Kubernetes treat it as string
             if ("request" in context){
                 context.request = JSON.stringify(context.request).replace(/\{/g, '\\{').replace(/}/g, '\\}');
             }
